52aff8dd902f5993b9a7a352a26d48bb507c5e8f
Joe Vargas <joevargas92@gmail.com>
2019-03-02 22:04:00 -0800

    base

--- src/Day1.hs
+++ src/Day1.hs
@@ -6,14 +6,36 @@ import GridProto.Core
 main :: IO ()
 main = runClassic classic
 
-classic :: Classic (Int, Int)
+data State = State
+  { gameMode :: GameMode
+  , playState :: PlayState
+  } deriving (Show, Eq)
+
+data GameMode
+  = GameMode'Start
+  | GameMode'Play
+  | GameMode'GameOver
+  deriving (Show, Eq)
+
+data PlayState = PlayState
+  { player :: Player
+  } deriving (Show, Eq)
+
+newtype RoomIndex = RoomIndex Int deriving (Show, Eq, Num)
+
+data Player = Player
+  { playerPos :: (Int, Int)
+  , playerRoom :: RoomIndex
+  } deriving (Show, Eq)
+
+classic :: Classic State
 classic = Classic
   { title = "Day 1"
-  , rows = sides
-  , cols = sides
-  , tilePixelSize = 64
+  , rows = screenH
+  , cols = screenW
+  , tilePixelSize = 24
   , backgroundColor = Black2
-  , setupFn = return (0,0)
+  , setupFn = return initState
   , updateFn = update
   , cleanupFn = const (return ())
   , tileMapFn = tileMap
@@ -21,21 +43,63 @@ classic = Classic
   , quitFn = const False
   }
 
-sides :: Int
-sides = 8
+screenW, screenH :: Int
+screenW = 45
+screenH = 30
 
-update :: Input -> (Int, Int) -> IO (Int, Int)
-update Input{keys=keys} (x,y) = return (x',y')
+initState :: State
+initState = State
+  { gameMode = GameMode'Play
+  , playState = PlayState (Player (0,0) 4)
+  }
+
+update :: Input -> State -> IO State
+update input st = case gameMode st of
+  GameMode'Start -> return st
+  GameMode'Play -> return $ st
+    { playState = (playState st)
+      { player = updatePlayer input (player (playState st)) } }
+  GameMode'GameOver -> return st
+
+updatePlayer :: Input -> Player -> Player
+updatePlayer input p = p
+  { playerPos = updatePlayerPos input (playerPos p) }
+
+updatePlayerPos :: Input -> (Int, Int) -> (Int, Int)
+updatePlayerPos Input{keys=keys} (x,y) = (x',y')
   where
     x'
-      | lookupKey keys LeftArrow == Pressed = x - 1
-      | lookupKey keys RightArrow == Pressed = x + 1
+      | left == Pressed || left == Held = x - 1
+      | right == Pressed || right == Held = x + 1
       | otherwise = x
     y'
-      | lookupKey keys UpArrow == Pressed = y - 1
-      | lookupKey keys DownArrow == Pressed = y + 1
+      | up == Pressed || up == Held = y - 1
+      | down == Pressed || down == Held = y + 1
       | otherwise = y
+    left = lookupKey keys LeftArrow
+    right = lookupKey keys RightArrow
+    up = lookupKey keys UpArrow
+    down = lookupKey keys DownArrow
 
-tileMap :: (Int, Int) -> Map (Int, Int) Tile
-tileMap xy = fromList [(xy, Tile Nothing Nothing (Just rd0))]
+tileMap :: State -> Map (Int, Int) Tile
+tileMap st = case gameMode st of
+  GameMode'Start -> fromList []
+  GameMode'Play -> mergeTiles clear $ fromList [ ( (x,y), Tile Nothing Nothing (Just wh1) ) ]
+  GameMode'GameOver -> fromList []
+  where
+    (x,y) = playerPos (player (playState st))
+    clear = clearTileMap (colorFromRoomIndex (playerRoom (player (playState st))))
+
+clearTileMap :: Color -> Map (Int, Int) Tile
+clearTileMap c = fromList
+  [ ((x,y), Tile Nothing Nothing (Just c))
+  | y <- [0..pred screenH]
+  , x <- [0..pred screenW]
+  ]
+
+colorFromRoomIndex :: RoomIndex -> Color
+colorFromRoomIndex (RoomIndex idx) = colors !! (idx `mod` len)
+  where
+    colors = colorWheel2
+    len = length colors
 
